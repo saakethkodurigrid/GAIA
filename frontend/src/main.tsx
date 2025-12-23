e1a167e7e1f61d0a564b049a20b84a0db87c371a
Abhishek Kumar <abhkumar@griddynamics.com>
2025-12-23 16:40:48 +0530

    Remove unused App1 and App2 components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import App from './App.tsx'
-import App2 from './App2.tsx'
-// import App1 from './App1.tsx'
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    {/* <App1/> */}
-    {/* <App2/> */}
+
+
   </StrictMode>,
 )
